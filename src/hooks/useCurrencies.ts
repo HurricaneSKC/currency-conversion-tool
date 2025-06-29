fb998fa497cf9c64374322c0a7437d40b324010a
Sanjay Chopra <sanjaykchopra@gmail.com>
2025-06-29 22:21:46 +0100

    adds in hook to get currencies

--- src/hooks/useCurrencies.ts
+++ src/hooks/useCurrencies.ts
@@ -1,15 +1,56 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
+// sample model data
+
+// "0": {
+//     "id": 1,
+//     "name": "UAE Dirham",
+//     "short_code": "AED",
+//     "code": "784",
+//     "precision": 2,
+//     "subunit": 100,
+//     "symbol": "د.إ",
+//     "symbol_first": true,
+//     "decimal_mark": ".",
+//     "thousands_separator": ","
+// },
+// "1": {
+//     "id": 2,
+//     "name": "Afghani",
+//     "short_code": "AFN",
+//     "code": "971",
+//     "precision": 2,
+//     "subunit": 100,
+//     "symbol": "؋",
+//     "symbol_first": false,
+//     "decimal_mark": ".",
+//     "thousands_separator": ","
+// },
+
+export interface Currency {
+  id: number;
+  name: string;
+  short_code: string;
+  code: string;
+  precision: number;
+  subunit: number;
+  symbol: string;
+  symbol_first: boolean;
+  decimal_mark: string;
+  thousands_separator: string;
+}
+
 const useCurrencies = () => {
-  const fetchCurrencies = () => {
-    axios.get('https://api.currencybeacon.com/v1/currencies', {
-        params: { api_key: import.meta.env.VITE_CURRENCY_BEACON_API }}
-    ).then(
-      result => result.data
-    )
+  const fetchCurrencies = (): Promise<Currency[]> => {
+    return axios.get<{ [key: string]: Currency; }>('https://api.currencybeacon.com/v1/currencies', {
+      params: { api_key: import.meta.env.VITE_CURRENCY_BEACON_API }
+    })
+      .then(
+        result => Object.values(result.data)
+      );
   }
-  return useQuery({
+  return useQuery<Currency[]>({
     queryKey: ['currencies'],
     queryFn: fetchCurrencies
   })
